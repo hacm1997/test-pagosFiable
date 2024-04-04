f949514ae158c42bd4a51dddbc7d1d173bd50b94
heinerkru <heiner@kru360.com>
2024-04-03 22:54:31 -0500

    config origin cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,16 @@ require('./Shared/Database/mongodb');
 
 const productRoutes = require('./product/rest/product.controller');
 const customerRoutes = require('./customer/rest/customer.controller');
+
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
 app.use('/', productRoutes);
 app.use('/', customerRoutes);
 
